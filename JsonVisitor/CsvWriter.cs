1016c7e55128fe3e5c4c6cd3bb5a6cc255e86299
Ryan Falzon <ryan@hash.consulting>
2021-07-20 12:22:53 +0200

    Updated procedure

---
+++ JsonVisitor/CsvWriter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace JsonVisitor
+{
+    public static class CsvWriter
+    {
+        public static void Write(List<Dictionary<string, object>> data, List<string> properties, string path)
+        {
+            try
+            {
+                var csvData = (from entry in data
+                               select (from property in properties
+                                       select entry.ContainsKey(property) ? entry[property]?.ToString() : null).ToList()).ToList();
+
+                csvData.Insert(0, properties.Select(property => property).ToList());
+
+                WriteToFile(csvData, path);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+        private static void WriteToFile(List<List<string>> data, string path)
+        {
+            var stringBuilder = new StringBuilder();
+
+            foreach (var @object in data)
+            {
+                stringBuilder.Append(string.Join(',', @object));
+                stringBuilder.AppendLine();
+            }
+
+            File.WriteAllText(path, stringBuilder.ToString());
+        }
+    }
+}
--- JsonVisitor/Program.cs
+++ JsonVisitor/Program.cs
@@ -1,63 +1,103 @@
-﻿using Newtonsoft.Json.Linq;
+﻿using JsonVisitor;
+using Newtonsoft.Json.Linq;
 using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace JsonIterator
 {
     public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             try
             {
                 if (args.Length == 0)
                 {
-                    throw new Exception("Path must be passed as an argument!");
+                    throw new Exception("JSON path and CSV path must be passed as an argument!");
+                }
+                else if(args.Length == 1)
+                {
+                    throw new Exception("CSV path must be passed as an argument!");
+                }
+                else if(args.Length > 2)
+                {
+                    throw new Exception("Invalid number of arguments passed!");
                 }
 
-                var json = File.ReadAllText(args[0]);
-                var parsedJson = JToken.Parse(json);
+                Console.WriteLine("Attempting to parse JSON file...\n");
+                Console.ForegroundColor = ConsoleColor.Green;
 
-                Visit(parsedJson, string.Empty);
+                var json = File.ReadAllText(args[0]);
+                var parsedJson = JArray.Parse(json);
+
+                var result = Visit(parsedJson);
+                var propertyNames = result.Select(item => item.Select(item => item.Name)).SelectMany(item => item).Distinct().ToList();
+                var rows = result.Select(item => item.ToDictionary(item => item.Name, item => (object)item.Value)).ToList();
+
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("\nJSON file parsed successfully...");
+
+                CsvWriter.Write(rows, propertyNames, args[1]);
+
+                Console.WriteLine("CSV file created successfully!\n\nPress any key to exit....");
+                Console.ReadLine();
             }
             catch (Exception ex)
             {
+                Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine(ex.Message);
             }
         }
 
-        public static void Visit(JToken jToken, string parentName)
+        private static List<List<Property>> Visit(JArray jArray)
         {
-            if (jToken is JObject)
-            {
-                Visit(jToken as JObject, parentName);
-            }
-            else if (jToken is JArray)
-            {
-                Visit(jToken as JArray, parentName);
-            }
-            else
-            {
-                Console.WriteLine($"{parentName} - {jToken}");
-            }
-        }
+            var rows = new List<List<Property>>();
 
-        public static void Visit(JObject jObject, string parentName)
-        {
-            foreach (var property in jObject.Properties())
-            {
-                Visit(property.Value, $"{parentName}{(!string.IsNullOrWhiteSpace(parentName) ? ":" : string.Empty)}{property.Name}");
-            }
-        }
-
-        public static void Visit(JArray jArray, string parentName)
-        {
             var counter = 0;
             foreach (var child in jArray.Children())
             {
-                Visit(child, $"{parentName}[{counter}]");
+                rows.Add(Visit(child, string.Empty));
                 counter++;
             }
+
+            return rows;
+        }
+
+        private static List<Property> Visit(JToken jToken, string parentName)
+        {
+            var properties = new List<Property>();
+
+            if (jToken is JObject)
+            {
+                foreach (var property in (jToken as JObject).Properties())
+                {
+                    properties.AddRange(Visit(property.Value, $"{parentName}{(!string.IsNullOrWhiteSpace(parentName) ? ":" : string.Empty)}{property.Name}"));
+                }
+            }
+            else if (jToken is JArray)
+            {
+                var counter = 0;
+                foreach (var child in (jToken as JArray).Children())
+                {
+                    properties.AddRange(Visit(child, $"{parentName}[{counter}]"));
+                    counter++;
+                }
+            }
+            else
+            {
+                var property = new Property()
+                {
+                    Name = parentName,
+                    Value = jToken.ToString()
+                };
+
+                properties.Add(property);
+                Console.WriteLine($"\t{property}");
+            }
+
+            return properties;
         }
     }
 }
